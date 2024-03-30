27ce60b89725a4f48c351e343f9f53eb7864e113
Kumari <diksha.h.kumari@accenture.com>
2024-03-30 21:52:46 +0530

    half Pyaramid Number

---
+++ halfPyramidNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+#include<iostream>
+int main(){
+
+int i,j,n;
+std::cin>>n;
+for(i=1;i<=n;i++){
+for(j=1;j<=n;j++){
+if(j<=i){
+std::cout<<i ;
+}
+
+}
+
+
+std::cout<<"\n";
+} 
+return 0;
+}
